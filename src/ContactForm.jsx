1c9ac8d073364194ed880e7c9ca9df7a710372bb
Julia Mazzoni <juliarmazzoni@gmail.com>
2025-01-08 14:49:47 +1100

    Add form validation

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
+import validate from "./utils/validate";
 
 export const ContactForm = ({setMessageSent}) => {
 
   const [radioOption, setRadioOption] = useState('')
+  const [formData, setFormData] = useState({})
+  const [errors, setErrors] = useState({})
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessageSent(true)
-    console.log('its working')
+    const validationErrors = validate(formData)
+
+    if(Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors)
+
+    } else {
+      setMessageSent(true)
+      setErrors({})
+    }
   }
 
   const handleGeneralEnquireClick = () => {
     setRadioOption('general')
   }
-
   const handleSupportRequestClick = () => {
     setRadioOption('support')
+  }
+  const handleOnChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
   }
 
   return (
@@ -28,18 +40,22 @@ export const ContactForm = ({setMessageSent}) => {
 
           <div className="firstName-lastName">
             <div className="first-name">
-              <label name='first-name'>First Name <span className="green">*</span></label>
-              <input type="text" />
+              <label >First Name <span className="green">*</span></label>
+              <input type="text" name='firstName' onChange={handleOnChange} />
+              {errors.firstName && <span className="errors">{errors.firstName}</span>}
             </div>
+
             <div className="last-name">
-              <label name='last-name'>Last Name <span className="green">*</span></label>
-              <input type="text" />
+              <label >Last Name <span className="green">*</span></label>
+              <input type="text" name='lastName' onChange={handleOnChange} />
+              {errors.lastName && <span className="errors">{errors.lastName}</span>}
             </div>
           </div>
 
           <div className="email-address">
-            <label name='email'>Email Address <span className="green">*</span></label>
-            <input type="email" />
+            <label >Email Address <span className="green">*</span></label>
+            <input type="email" name='email' onChange={handleOnChange} />
+            {errors.email && <span className="errors">{errors.email}</span>}
           </div>
 
           <div className="query-type">
@@ -47,23 +63,28 @@ export const ContactForm = ({setMessageSent}) => {
 
             <div className="query-options">
               <div className={radioOption === 'general' ? 'green-background' : 'transparent'}>
-                <input type="radio" name='query-type' onClick={handleGeneralEnquireClick}/> General Enquiry
+                <input type="radio" name='queryType' value='general-enquire' onClick={handleGeneralEnquireClick} onChange={handleOnChange}  /> General Enquiry
               </div>
               <div className={radioOption === 'support' ? 'green-background' : 'transparent'}>
-                <input type="radio" name='query-type' onClick={handleSupportRequestClick} /> Support Request 
+                <input type="radio" name='queryType' value='support-request' onClick={handleSupportRequestClick} onChange={handleOnChange}  /> Support Request 
               </div>
-            
+              {errors.queryType && <span className="errors">{errors.queryType}</span>}
             </div>
           </div>
 
           <div className="message">
-            <label name='message'>Message <span className="green">*</span></label>
-            <textarea name="message" type='text'></textarea>
+            <label >Message <span className="green">*</span></label>
+            <textarea name="message" type='text' onChange={handleOnChange}  ></textarea>
+            {errors.message && <span className="errors">{errors.message}</span>}
           </div>
 
-          <div className="checkbox">
-            <input type="checkbox" /> <p>I consent to being contacted by the team</p> <span className="green">*</span>
+          <div>
+            <div className="checkbox">
+              <input type="checkbox" name="consent" onChange={handleOnChange} /> <p>I consent to being contacted by the team</p> <span className="green">*</span>
+            </div>
+            {errors.consent && <span className="errors">{errors.consent}</span>}
           </div>
+          
 
           <button className="submit-button" type="submit" >Submit</button>
 
---
+++ src/utils/validate.js
@@ -0,0 +1,30 @@
+ 
+ export default function validate(formData) {
+  console.log(formData)
+  const errors = {}
+  if(!formData.firstName){
+    errors.firstName = "First name is required."
+  }
+
+  if(!formData.lastName){
+    errors.lastName = "Last name is required."
+  }
+
+  if(!formData.email){
+    errors.email = "Please enter a valid email address."
+  }
+
+  if(!formData.queryType){
+    errors.queryType = "Please select a query type."
+  }
+
+  if(!formData.message){
+    errors.message = "Message is required."
+  }
+
+  if(!formData.consent){
+    errors.consent = "To submit this form, please consent to being contacted."
+  }
+
+  return errors
+}
